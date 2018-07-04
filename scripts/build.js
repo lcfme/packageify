04401477bdb782e41de5d64dbd2bc13fe3a979fd
lcfme <lcfme@sina.com>
2018-07-04 14:55:52 +0800

    done

---
+++ scripts/build.js
@@ -0,0 +1,94 @@
+var path = require('path');
+var child_process = require('child_process');
+var shell = require('shelljs');
+var chalk = require('chalk');
+var readdir = require('recursive-readdir');
+var projectrcUtil = require('./projectrc-utils');
+var projectrc = projectrcUtil.getProjectrc();
+var log = console.log.bind(console);
+
+function getDistName(filename) {
+    var relative = path.relative(projectrc.src, filename);
+    return path.resolve(projectrc.dist, relative);
+}
+
+function exec(cmd) {
+    return child_process.exec(cmd).on('close', function(code) {
+        if (code) {
+            log(chalk.red(`failed run: ${cmd}`));
+            throw new Error(`failed run: ${cmd}`);
+        }
+        log(chalk.green(`complete : ${cmd}`));
+    });
+}
+
+var bundleLibJsCmd = (function(jsLib) {
+    var targetName;
+    if (!jsLib) return null;
+    return Object.keys(jsLib).map(libName => {
+        targetName = path.resolve(projectrc.browserify.libDir, libName);
+        if (!/\.jsx?$/.test(targetName)) targetName = targetName + '.js';
+        return (
+            'browserify' +
+            jsLib[libName].reduce((r, dep) => {
+                return r + ' -r ' + dep;
+            }, '') +
+            ' > ' +
+            targetName
+        );
+    });
+})(projectrcUtil.getJsLib());
+
+var jsLibInPlainArray = projectrcUtil.getJsLibInPlainArray();
+var jsExternalString = (function() {
+    var first;
+    return jsLibInPlainArray.reduce((r, dep) => {
+        first = dep.search(':');
+        if (first > -1) dep = dep.substr(++first);
+        return r + ' -x ' + dep;
+    }, '');
+})();
+
+/**
+ * 打包公用库
+ */
+shell.mkdir('-p', path.resolve(projectrc.browserify.libDir));
+bundleLibJsCmd.forEach(cmd => {
+    exec(cmd);
+});
+
+/**
+ * 遍历文件
+ */
+
+readdir(projectrc.src, function(err, files) {
+    if (err) throw new Error('Error happened when readdir error.');
+    files.forEach(file => {
+        var dirname = path.dirname(file),
+            destname = getDistName(file);
+        if (/\.entry\.jsx?$/i.test(file)) {
+            destname = destname.replace(/\.entry\.jsx?$/i, '.js');
+            var cmd = `browserify ${jsExternalString} -e ${file} > ${destname}`;
+            var distdir = path.dirname(destname);
+            shell.mkdir('-p', distdir);
+            exec(cmd);
+        } else if (/\.less$/i.test(file)) {
+            destname = destname.replace(/\.less$/i, '.css');
+            var cmd = `lessc --clean-css ${file} > ${destname}`;
+            var distdir = path.dirname(destname);
+            shell.mkdir('-p', distdir);
+            exec(cmd);
+        } else if (/\.html$/i.test(file)) {
+            var cmd = `html-img-loader -f ${file} -s 50 > ${destname}`;
+            var distdir = path.dirname(destname);
+            shell.mkdir('-p', distdir);
+            exec(cmd);
+        } else if (!/^\.(html)|(less)|(jsx?)$/.test(file)) {
+            var distdir = path.dirname(destname);
+            shell.mkdir('-p', distdir);
+            shell.cp('-f', file, destname);
+        } else {
+            log(chalk.yellow(`unresolved resorce: ${file}`));
+        }
+    });
+});
---
+++ scripts/projectrc-utils.js
@@ -0,0 +1,79 @@
+var path = require('path');
+var _ = require('lodash');
+var readJson = require('readjson');
+var cwd = process.cwd();
+var projectrc;
+
+try {
+    projectrc = readJson.sync(resolve('./projectrc.json'));
+} catch (err) {
+    throw new Error(
+        `Cannot find projectrc.json in current work dir. Please ensure your are in project root folder.`
+    );
+}
+
+/**
+ * valid projectrc
+ */
+if (!_.isPlainObject(projectrc) || !projectrc.src || !projectrc.dist)
+    throw new Error(
+        `projectrc.json is invalid. projectrc must has dist, src schema to specify your source foler and output folder`
+    );
+    projectrc.src = resolve(projectrc.src);
+    projectrc.dist = resolve(projectrc.dist);
+
+if (_.isPlainObject(projectrc.browserify)) {
+    (function() {
+        var config = projectrc.browserify,
+            currentLib;
+        if (!config.libDir)
+            throw new Error(
+                'projectrc.browserify is invalid. Please ensure it has libDir schema to specify output lib dir.'
+            );
+        config.libDir = resolve(projectrc.dist, config.libDir);
+        if (_.isPlainObject(config.lib)) {
+            Object.keys(config.lib).forEach(libName => {
+                if (!_.isArray(config.lib[libName]))
+                    throw new Error(
+                        'projectrc.lib[libNare] must be type Array'
+                    );
+                config.lib[libName] = config.lib[libName].map(depsName => {
+                    if (!depsName)
+                        throw new Error('depsName cannot be undefined');
+                    try {
+                        require.resolve(depsName);
+                        return depsName;
+                    } catch (err) {
+                        return resolve(projectrc.src, depsName);
+                    }
+                });
+            });
+        }
+    })();
+}
+
+function resolve(...args) {
+    return path.resolve(cwd, ...args);
+}
+
+exports.getProjectrc = function() {
+    return projectrc;
+};
+
+exports.getJsLib = function() {
+    try {
+        return projectrc.browserify.lib;
+    } catch (err) {
+        return null;
+    }
+};
+
+exports.getJsLibInPlainArray = function() {
+    try {
+        return Object.keys(projectrc.browserify.lib).reduce((result, deps) => {
+            return [].concat(result, projectrc.browserify.lib[deps]);
+        }, []);
+    } catch (err) {
+        return null;
+    }
+};
